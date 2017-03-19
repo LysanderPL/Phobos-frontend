792df888cfac0a94c455d2a62841fe591ead54c6
LysanderPL <m.kruszewski@sunflux.pl>
2017-03-19 23:48:28 +0100

    login component

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by maciej on 18.03.17.
+ */
+import {Component} from "@angular/core";
+@Component({
+    selector: 'login',
+    templateUrl: 'login.component.html'
+})
+export class LoginComponent {
+
+}
