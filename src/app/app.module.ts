5136bd695e57e6e37492531b88244bc44a9b5038
LysanderPL <m.kruszewski@sunflux.pl>
2017-03-19 23:49:34 +0100

    loading component to main app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {TopNavbarComponent} from "./navbar/top-navbar.component";
+import {SideNavbarComponent} from "./navbar/side-navbar.component";
+import {LoginComponent} from "./login/login.component";
+import {OverviewComponent} from "./overview/overview.component";
+import {NotFoundComponent} from "./not-found.component";
+import {AppRoutingModule} from "./app-routing.module";
+import {ResourceComponent} from "./resource/resource.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        TopNavbarComponent,
+        SideNavbarComponent,
+        LoginComponent,
+        OverviewComponent,
+        NotFoundComponent,
+        ResourceComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent, ResourceComponent, TopNavbarComponent, SideNavbarComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
