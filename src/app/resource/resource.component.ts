a39603a6aadddbc6036214bfb64a6f2500857063
LysanderPL <m.kruszewski@sunflux.pl>
2017-03-19 23:47:38 +0100

    resources component

---
+++ src/app/resource/resource.component.ts
@@ -0,0 +1,16 @@
+/**
+ * Created by maciej on 19.03.17.
+ */
+import {Component} from "@angular/core";
+@Component({
+    selector: 'resource-bar',
+    templateUrl: 'resource.component.html'
+})
+export class ResourceComponent {
+
+    private Uranium = 0;
+    private Ferrum = 0;
+    private Silicon = 0;
+    private Helium = 0;
+
+}
