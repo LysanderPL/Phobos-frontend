38915a9333afffee69312a40df0c2b3b12cdaa74
LysanderPL <m.kruszewski@sunflux.pl>
2017-03-19 23:47:21 +0100

    navbars components

---
+++ src/app/navbar/side-navbar.component.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by maciej on 19.03.17.
+ */
+import {Component, Directive} from "@angular/core";
+
+@Component({
+    selector: 'side-navbar',
+    templateUrl: 'side-navbar.component.html'
+})
+export class SideNavbarComponent {
+
+ }
---
+++ src/app/navbar/top-navbar.component.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by maciej on 18.03.17.
+ */
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'top-navbar',
+    templateUrl: 'top-navbar.component.html'
+})
+export class TopNavbarComponent {
+
+}
