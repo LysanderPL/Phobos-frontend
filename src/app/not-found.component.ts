6a2dd5d4d270d18452709b8033eb9a2643d24465
LysanderPL <m.kruszewski@sunflux.pl>
2017-03-19 23:48:40 +0100

    not found component

---
+++ src/app/not-found.component.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by maciej on 19.03.17.
+ */
+import { Component } from '@angular/core';
+
+@Component({
+    template: '<h2>Page not found</h2>'
+})
+export class NotFoundComponent {}
+
