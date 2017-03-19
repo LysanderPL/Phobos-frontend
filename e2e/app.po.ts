95914a4ebc503b0e5deba9b7874408cb572304cd
LysanderPL <m.kruszewski@sunflux.pl>
2017-03-19 23:51:02 +0100

    app po

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -6,6 +6,6 @@ export class PhobosFrontendPage {
   }
 
   getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+    return element(by.css('app-overview h1')).getText();
   }
 }
