2da62c60233bdeba48c52a7d56ed30bb57ccc8cb
LysanderPL <m.kruszewski@sunflux.pl>
2017-04-02 20:42:48 +0200

    Fixing error on slashes in filter

--- src/app/library/pipes/buildings.pipe.ts
+++ src/app/library/pipes/buildings.pipe.ts
@@ -2,6 +2,7 @@
  * Created by maciej on 23.03.17.
  */
 import {Pipe} from "@angular/core";
+import {StringTools} from "../string.tools";
 
 @Pipe({
     name: 'BuildingsPipe'
@@ -10,6 +11,7 @@ export class BuildingsPipe {
 
     transform(value, args?): boolean {
         return value.filter(buildingsList => {
+            args = StringTools.addslashes(args);
             if (args == '' || args == null) return true;
             return buildingsList.name.search(args) >= 0 || buildingsList.name.toUpperCase().search(args) >= 0 || buildingsList.name.toLowerCase().search(args) >= 0;
         });
